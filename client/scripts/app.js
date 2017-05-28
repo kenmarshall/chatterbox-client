5b377241fcb860bc19a1823e815093e9a9ade71f
Kenneth Marshall <kdmarshall89@gmail.com>
2017-05-27 19:13:07 -0500

    Implement most basic requirements
    Install and use bootstrap for styling with bower
    Add google font with bower
    Add filterby roomname functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,159 @@
 // YOUR CODE HERE:
+var app = {};
+
+
+app.init = function(){
+  // html elements
+  app.$chats = $('#chats');
+  app.$username = $('.username');
+  app.$text = $('.text');
+  app.$chatTemplate = $('#chatTemplate');
+  app.$roomNameSelections = $('#roomNameSelections');
+
+
+
+  app.messages = [];
+  app.roomNameLists = ['Lobby' , '4Chan', 'All'];
+  app.currentRoomname = 'All';
+  app.currentUsername = window.location.search.split('=')[1];
+
+};
+
+app.updateRoomSelectionList = function() {
+
+}
+
+
+
+app.send = function (msg, cb) {
+
+
+
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(msg),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent', data);
+      cb('success');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      cb('fail');
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = function (roomName) {
+
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages',
+    type: 'GET',
+    data : {order: '-createdAt'},
+    contentType: 'application/json',
+    success: function (data) {
+      if (!data.results) {
+        return;
+      }
+
+      app.messages = data.results;
+      app.clearMessages();
+      app.messages.forEach(app.renderMessage);
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.clearMessages = function () {
+  app.$chats.html('');
+}
+
+app.renderMessage = function (message) {
+  var $msg = app.$chatTemplate.clone(true);
+  $msg.find('.username').text(message.username);
+  $msg.find('.text').text(message.text);
+  $('#chats').append($msg);
+}
+
+app.filterMessagesByRoom = function () {
+ var filteredmsgs = app.messages.filter(function(message){
+    if (message.roomname === app.currentRoomname.toLowerCase()){
+        return true;
+    }
+  });
+
+  console.log(filteredmsgs);
+
+  if (filteredmsgs.length) {
+    app.clearMessages();
+    filteredmsgs.forEach(app.renderMessage);
+  }
+
+
+}
+
+
+
+
+  // ======================= click handlers ===================================
+$(document).ready(function(){
+  app.init();
+  app.fetch();
+
+  $('#refreshMessages').on('click', function(event) {
+    app.fetch();
+  });
+
+  $('#sendMessage').submit(function(event){
+   // var msg = $()$('#message').val());
+   if ($('#messageText').val().length ) {
+      var msg  = {};
+
+      msg.username = app.currentUsername;
+      msg.text = $('<div></div>').text($('#messageText').val()).text();
+      msg.roomname = app.currentRoomname;
+      app.send(msg, function(status){
+        if (status === 'success') {
+          app.fetch();
+        }
+      });
+    }
+
+    event.preventDefault();
+  });
+
+  $('#roomNameSelections li').on('click', function(){
+    app.currentRoomname = $(this).text().toLowerCase();
+    $('#roomNameIndicator').html($(this).text() + ' <span class="caret"></span>');
+    if (app.currentRoomname === 'All') {
+      app.fetch();
+    } else {
+
+      app.filterMessagesByRoom();
+    }
+
+  });
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+/*
+
+*/
